6c6d79f986bcab32a0dc0b3ed7f87f436ebb79d6
Aditya Palange <adityapalange@gmail.com>
2022-08-14 16:01:55 +0530

    Add feature 1

--- app/src/main/java/com/adycoder/cicd1/MainActivity.java
+++ app/src/main/java/com/adycoder/cicd1/MainActivity.java
@@ -1,18 +1,28 @@
 package com.adycoder.cicd1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+
+import com.adycoder.cicd1.databinding.ActivityMainBinding;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding mAMBinding = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
 
+        mAMBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
 
+        mAMBinding.sayHelloButton.setOnClickListener(view ->{
+            String helloMessage = mAMBinding.nameEditText.getText().toString().trim();
+            mAMBinding.helloTextView.setText(helloMessage);
+        });
 
 
 
